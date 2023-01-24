531edef147eb2ac3de9b181ec5e0a03f3b03f9a2
Dexter Louie Aniez <dexterlouie.aniez@sun-asterisk.com>
2023-01-24 10:22:33 +0800

    4.1 Admin add word page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,5 +2,9 @@ import LoginPage from "./public/login"
 import SignupPage from "./public/signup"
 import DashboardPage from "./protected/dashboard"
 import ProfilePage from "./protected/profile"
+import WordPage from "./protected/word"
 
-export {LoginPage, SignupPage, DashboardPage, ProfilePage}
+export {
+  LoginPage, SignupPage, DashboardPage, 
+  ProfilePage, WordPage
+}
