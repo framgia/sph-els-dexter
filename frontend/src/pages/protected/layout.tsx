6feae7747b1701ed91741799804952f9759eaf43
Dexter Louie Aniez <dexterlouie.aniez@sun-asterisk.com>
2023-01-26 10:57:45 +0800

    6.1 Create user category page list (created logout api)

--- frontend/src/pages/protected/layout.tsx
+++ frontend/src/pages/protected/layout.tsx
@@ -3,9 +3,15 @@ import {Location, useLocation, Link} from "react-router-dom"
 import {useSelector} from "react-redux"
 import {Cookies} from "react-cookie"
 import Modal from "react-modal"
+import {AxiosResponse} from "axios"
 import {RootState} from "./../../redux"
 import {slices} from "./../../redux/slice-collection"
-import {ERouteNames} from "./../../enums"
+import {ERouteNames, EEndpoints} from "./../../enums"
+import {IApiResponse} from "./../../types"
+import {api} from "./../../configs"
+import {useToast} from "./../../hooks"
+import {IUserDetails} from "../../redux/user/user-states"
+import {verifyToken} from "../../utils"
 
 Modal.setAppElement("#root")
 
@@ -14,6 +20,7 @@ interface ILayoutProps {
 }
 
 const ProtectedLayout: React.FC<ILayoutProps> = ({children}: ILayoutProps) => {
+  const {showToast} = useToast()
   const location: Location = useLocation()
   const cookie = new Cookies()
 
@@ -33,14 +40,35 @@ const ProtectedLayout: React.FC<ILayoutProps> = ({children}: ILayoutProps) => {
     ? "Category"
     : null
 
-    const signOut = () => {
-      /** Call logout API here */
+    const reset = () => {
       cookie.remove("refresh_token")
 
       slices.datalog.reset()
       slices.session.logout()
 
       window.location.href = "/"
+    }
+
+    const signOut = async () => {
+      try {
+        const refreshToken = cookie.get("refresh_token")
+
+        if (!refreshToken) return reset()
+
+        const userDetails: IUserDetails = verifyToken(refreshToken) as IUserDetails
+
+        if (!userDetails) return reset()
+
+        const {data: {message}}: AxiosResponse<IApiResponse<never>> = await api.post(EEndpoints.LOGOUT, {email: userDetails.email})
+      
+        showToast("success", message)
+        reset()
+      } catch (err) {
+        const error: Error = err as Error
+
+        console.error(err)
+        showToast("error", error.message ?? "Unable to logout, please check the logs for more details.")
+      }
     }
 
   return (
